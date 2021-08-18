fc62a9f5176578a0515173ee918cb68787464387
dangerfrog <80552727+dangerfrog@users.noreply.github.com>
2021-08-19 11:49:00 +1200

    basic Plate app

--- packages/editor/src/components/base-editor/index.tsx
+++ packages/editor/src/components/base-editor/index.tsx
@@ -4,12 +4,5 @@ import { Plate } from "@udecode/plate";
 export interface BaseEditorProps {}
 
 export const BaseEditor: React.FC<BaseEditorProps> = (props) => {
-  const editableProps = {
-    placeholder: "Type…",
-    style: {
-      padding: "15px",
-    },
-  };
-
-  return <Plate id="1" editableProps={editableProps} />;
+  return <Plate />;
 };
