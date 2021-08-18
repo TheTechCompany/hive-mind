79253f402302502c43b1b6e6b961b3d70342bc48
dangerfrog <80552727+dangerfrog@users.noreply.github.com>
2021-08-19 11:27:12 +1200

    Trying to set up Slate

--- packages/editor/src/components/base-editor/index.tsx
+++ packages/editor/src/components/base-editor/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Plate } from "@udecode/plate";
 
-export interface BaseEditorProps {
+export interface BaseEditorProps {}
 
-}
+export const BaseEditor: React.FC<BaseEditorProps> = (props) => {
+  const editableProps = {
+    placeholder: "Type…",
+    style: {
+      padding: "15px",
+    },
+  };
 
-export const BaseEditor : React.FC<BaseEditorProps> = (props) => {
-    return (
-        <div>
-            base editor test live reload
-        </div>
-    );
-}
+  return <Plate id="1" editableProps={editableProps} />;
+};
