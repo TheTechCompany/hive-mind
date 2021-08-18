cc2790f97dd5a66471cae752eb7cb7619b0ab6b3
Ross Leitch <professional.balbatross@gmail.com>
2021-08-18 15:54:04 +1200

    Init

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import { BaseEditor } from '@hivemind/editor'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BaseEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/editor/src/components/base-editor/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export interface BaseEditorProps {
+
+}
+
+export const BaseEditor : React.FC<BaseEditorProps> = (props) => {
+    return (
+        <div>
+            base editor test live reload
+        </div>
+    );
+}
